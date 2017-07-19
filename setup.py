05dc5a700c4c6eddb4c5054e147b2ab091d0ef71
Mike Boers <github@mikeboers.com>
2017-07-19 15:19:24 -0400

    First pass.

---
+++ setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+
+setup(
+    
+    name='dirmap',
+    version='0.1.0',
+    description='Directory remapper.',
+    url='http://github.com/mikeboers/dirmap',
+    
+    packages=find_packages(exclude=['build*', 'tests*']),
+    include_package_data=True,
+    
+    author='Mike Boers',
+    author_email='dirmap@mikeboers.com',
+    license='BSD-3',
+    
+    classifiers=[
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 2',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ],
+    
+)
